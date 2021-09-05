bde9055e6caaea7468029a1b85c9db00d1b8034d
hamza887 <hamzabutt887@hotmail.com>
2021-09-05 04:39:50 -0700

    advanced array

--- hello.js
+++ hello.js
@@ -65,22 +65,42 @@
 // });
 // console.log(max);
 
-const temperature=[3,-2,-6,-1,9,'error',13,17,15,14,9,5];
-const hell=[1,2,6,8,3,27]
+// const temperature=[3,-2,-6,-1,9,'error',13,17,15,14,9,5];
+// const hell=[1,2,6,8,3,27]
 
-const calcTemperature=(temp,hell)=>{
-    let temps=temp.concat(hell)
-   // define a variable to store the values
-   console.log(temps);
-let max= temps[0];
-let min=temps[0];
-   //use a for loop or for each to check all the values
- for (let i = 0; i < temps.length; i++) {
-     if(typeof temps[i] !=='number') continue
-    if(temps[i]>max) max=temps[i];
-    if(temps[i]<min) min=temps[i]
- }
- console.log(max,min);
+// const calcTemperature=(temp,hell)=>{
+//     let temps=temp.concat(hell)
+//    // define a variable to store the values
+//    console.log(temps);
+// let max= temps[0];
+// let min=temps[0];
+//    //use a for loop or for each to check all the values
+//  for (let i = 0; i < temps.length; i++) {
+//      if(typeof temps[i] !=='number') continue
+//     if(temps[i]>max) max=temps[i];
+//     if(temps[i]<min) min=temps[i]
+//  }
+//  console.log(max,min);
 
+// }
+// calcTemperature(temperature,hell)
+
+
+//have to create a function which takes array as a paratmetn
+//use a for loop or for each 
+//have to string literals
+// console log the data
+
+const arr=[17,21,23];
+const printForeCast=(arr)=>{
+    let str=''
+    for (let i = 0; i < arr.length; i++) {
+        str = str+arr[i];
+        str=`...${str}C in ${i+1} days ...`
+    //  console.log(str);
+        
+    }
+    console.log(str);
 }
-calcTemperature(temperature,hell)
+printForeCast(arr)
+
