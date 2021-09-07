80e412928ef8c7f1be8eadeb257db44abbfd32f7
hamza887 <hamzabutt887@hotmail.com>
2021-09-07 08:37:46 -0700

    dom manipulation

---
+++ guessmynumber/script.js
@@ -0,0 +1,9 @@
+
+// console.log(document.querySelector('.between').textContent);
+
+// alert('hello I am an alert')
+ const hh= document.querySelector('.message');
+ hh.textContent= 'Correct Number ✨';
+
+ document.querySelector('.number').textContent= 13;
+ document.querySelector('.score').textContent=20
