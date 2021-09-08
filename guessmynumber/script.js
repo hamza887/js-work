a3a7ca437052e13b7564de1f5574f1e9c9f53ce8
hamza887 <hamzabutt887@hotmail.com>
2021-09-08 07:15:57 -0700

    hello game

--- guessmynumber/script.js
+++ guessmynumber/script.js
@@ -1,9 +1,16 @@
 
-// console.log(document.querySelector('.between').textContent);
+const guess= document.querySelector('.guess');
+const check= document.querySelector('.check');
+const message= document.querySelector('.message');
+const number=document.querySelector('.number');
 
-// alert('hello I am an alert')
- const hh= document.querySelector('.message');
- hh.textContent= 'Correct Number ✨';
 
- document.querySelector('.number').textContent= 13;
- document.querySelector('.score').textContent=20
+let random;
+   random= Math.floor(Math.random()*20);
+    console.log( number.value=random);
+
+
+check.addEventListener('click',()=>{
+    console.log('hello chekc');
+});
+
