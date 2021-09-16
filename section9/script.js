a4614cbfc5185cf4632fb02da542786346e7608d
hamza887 <hamzabutt887@hotmail.com>
2021-09-16 01:07:12 -0700

    hello js

--- section9/script.js
+++ section9/script.js
@@ -1,16 +1,16 @@
 
 
-const restraurant={
-    name:"Classico Italiano",
-    location: 'cia Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian','Pizzeria','Vegeterian','Organic'],
-    starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
-    mainMenu:['Pizza','pasta','risotto'],
+// const restraurant={
+//     name:"Classico Italiano",
+//     location: 'cia Angelo Tavanti 23, Firenze, Italy',
+//     categories: ['Italian','Pizzeria','Vegeterian','Organic'],
+//     starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
+//     mainMenu:['Pizza','pasta','risotto'],
 
-    order=(starterindex,mainindex)=>{
-      return[this.startMenu[startindex]+this.categories[mainindex]]
-    }
-};
+//     // order=(starterindex,mainindex)=>{
+//     //   return[this.startMenu[startindex]+this.categories[mainindex]]
+//     // }
+// };
 
 const arr=[2,3,4];
 const a =arr[0];
@@ -20,10 +20,34 @@ const [x,y,z]=arr;
 console.log(arr);
 console.log(x);
 
-const [first,  ,second]=restraurant.mainMenu
+// const [first,  ,second]=restraurant.mainMenu
 
-console.log(first,second);
+// console.log(first,second);
 
 // DESTRUCTING OBJECTS
 
+const arr3=[7,8,9];
+const arr2=[1,2,3,4];
+console.log([1,2,...arr2]);
+
+const restraurant={
+  name:"Classico Italiano",
+  location: 'cia Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian','Pizzeria','Vegeterian','Organic'],
+  starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
+  mainMenu:['Pizza','pasta','risotto'],
+
+  // order=(starterindex,mainindex)=>{
+  //   return[this.startMenu[startindex]+this.categories[mainindex]]
+  // }
+};
+
+const newMenu= [...restraurant.categories];
+
+const mainMenuCopy= [...restraurant.mainMenu]
+
+
+const menu= [...newMenu,...mainMenuCopy];
+
+console.log(menu);
 
