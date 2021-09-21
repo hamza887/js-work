0b9a7615e4234f878e6cde5c5d2bf5da0eba2158
hamza887 <hamzabutt887@hotmail.com>
2021-09-21 15:15:00 -0700

    hello js

--- section9/script.js
+++ section9/script.js
@@ -26,56 +26,56 @@ console.log(x);
 
 // DESTRUCTING OBJECTS
 
-const arr3=[7,8,9];
-const arr2=[1,2,3,4];
-console.log([1,2,...arr2]);
+// const arr3=[7,8,9];
+// const arr2=[1,2,3,4];
+// console.log([1,2,...arr2]);
 
-const restraurant={
-  name:"Classico Italiano",
-  location: 'cia Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian','Pizzeria','Vegeterian','Organic'],
-  starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
-  mainMenu:['Pizza','pasta','risotto'],
+// const restraurant={
+//   name:"Classico Italiano",
+//   location: 'cia Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian','Pizzeria','Vegeterian','Organic'],
+//   starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
+//   mainMenu:['Pizza','pasta','risotto'],
 
-  // order=(starterindex,mainindex)=>{
-  //   return[this.startMenu[startindex]+this.categories[mainindex]]
-  // }
-};
+//   // order=(starterindex,mainindex)=>{
+//   //   return[this.startMenu[startindex]+this.categories[mainindex]]
+//   // }
+// };
 
-const Menu=[...restraurant.starterMenu,...restraurant.mainMenu];
+// const Menu=[...restraurant.starterMenu,...restraurant.mainMenu];
 
-for(const item of Menu) console.log(item)
+// for(const item of Menu) console.log(item)
 
-const newMenu= [...restraurant.categories];
-const mainMenuCopy= [...restraurant.mainMenu]
-const menu= [...newMenu,...mainMenuCopy];
-console.log(menu);
+// const newMenu= [...restraurant.categories];
+// const mainMenuCopy= [...restraurant.mainMenu]
+// const menu= [...newMenu,...mainMenuCopy];
+// console.log(menu);
 
-const str= 'hamza';
-const letters=[...str].reverse();
+// const str= 'hamza';
+// const letters=[...str].reverse();
 
-const [Pizza,risotto,...otherfood]=[
-  ...restraurant.mainMenu,...restraurant.starterMenu
-]
-console.log(Pizza,risotto,otherfood);
+// const [Pizza,risotto,...otherfood]=[
+//   ...restraurant.mainMenu,...restraurant.starterMenu
+// ]
+// console.log(Pizza,risotto,otherfood);
 
-const add=(...numbers)=>{
-  let sum=0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-}
- add(2,3,4,5);
+// const add=(...numbers)=>{
+//   let sum=0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// }
+//  add(2,3,4,5);
 
- const xh=[23,2,4,5];
- add(...xh);
+//  const xh=[23,2,4,5];
+//  add(...xh);
 
-console.log(3|| 'jones');
+// console.log(3|| 'jones');
 
-const restraurantguests=22;
-const guests1= restraurantguests || 10;
-console.log(guests1);
+// const restraurantguests=22;
+// const guests1= restraurantguests || 10;
+// console.log(guests1);
 
 // const guest2= restraurantguests ?? 10;
 // console.log(guest2);
@@ -150,6 +150,28 @@ console.log(players1final);
 const printGoals=(...players)=>{
   
 }
+
+const arr3=[7,8,9];
+const arr2=[1,2,3,4];
+console.log([1,2,...arr2]);
+
+const restraurant={
+  name:"Classico Italiano",
+  location: 'cia Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian','Pizzeria','Vegeterian','Organic'],
+  starterMenu:['Focaccia','Bruschetta','Garlic bead','Caprese Salad'],
+  mainMenu:['Pizza','pasta','risotto'],
+
+  // order=(starterindex,mainindex)=>{
+  //   return[this.startMenu[startindex]+this.categories[mainindex]]
+  // }
+};
+
+const Menu=[...restraurant.starterMenu,...restraurant.mainMenu];
+
+for(const item of Menu) console.log(item);
+
+for(const [items,hh] of Menu.entries()) console.log(`${items+1}: ${hh}`)
 
 
 
