666398af37c95a2066a7832bf04594605a99ad99
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-02 13:46:55 -0400

    working on fucntions

--- functions.js
+++ functions.js
@@ -22,6 +22,59 @@ const jones={
     passport:234556
 }
 
-const checkIn=(flightNum,passenger)=>{
+const checkIn=(flightName,passport)=>{
+    flightName='13B';
+    passport.name='Mr '+passport.name
+    console.log(flightName,passport);
+    if(typeof passport.passport==='number') console.log('oh yaa you go t it right');
+    else console.log('you are not right');
+      
+}
+    checkIn(flight,jones);
+
+    console.log('NEXT TIMELINE STARTS');
+
+    const name=(myname)=>{
+       return myname.toUpperCase()
+    }
+
+    const whatIsYourName=(yourName)=>{
+       console.log(yourName,);
+    }
+    
+const oneWord =(str)=>{
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord=(str)=>{
+    const [first, ...others]=str.split(' ');
+    return [first.toUpperCase(),...others].join(' ');
+}
+
+const transformer=(str,fn)=>{
+    console.log(`Original string : ${str}  `);
+    console.log(`Original string : ${fn(str)}  `);
+
+    console.log(`Transformed by : ${fn.name}`);
 
 }
+transformer('JSidjhfksdj is the best ',upperFirstWord);
+
+
+//functions returning fucntions
+
+const greet=(greeting)=>{
+    return (name)=>{
+       console.log(`${greeting} ${name}`);
+   }
+}
+
+ const greethey= greet('hy');
+ greethey('hamza')
+ greethey('zaka')
+
+
+
+
+
+
