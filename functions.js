3a449b8f8c4174458dd109f337d8a50370a71f23
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-02 12:08:04 -0400

    wokring on the fucntions

--- functions.js
+++ functions.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const createBooking=(flightNum,numPassengers,price)=>{
-   
+const bookings=[];
+const createBooking=(flightNum,numPassengers=1,price=199*numPassengers)=>{
+//  numPassengers=numPassengers||1;
+//  price=price||"199$";
+//  flightNum=flightNum||"16B";
+ const booking={
+     flightNum,
+     numPassengers,
+     price
+ }
+ console.log(booking);
+ bookings.push(booking)
+}
+createBooking('12Bbb',2);
+console.log(bookings);
+
+const flight ='LH234';
+const jones={
+    name:'hamza zaka',
+    passport:234556
+}
+
+const checkIn=(flightNum,passenger)=>{
+
 }
