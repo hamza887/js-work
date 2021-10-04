6d2bea29ec4167481ade657bba2f9c3cd1442706
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-04 10:46:56 -0400

    hello arrays

--- Bankist-app/test.js
+++ Bankist-app/test.js
@@ -13,5 +13,23 @@ let kateDog=[];
      element<=5?juliaDog.push(element+' 🐶its a puppy '+(key)):juliaDog.push(element+' 🐶its a dog '+key)
  });
 
- console.log(juliaDog);
- console.log(kateDog);
+//  console.log(juliaDog);
+//  console.log(kateDog);
+
+const dogAges=[5,2,4,1,15,8,3];
+let humanDogs=[]
+ const calcAvergeDogAges=(ages)=>{
+   ages.map(a=> a<=2? humanDogs.push(2*a):humanDogs.push(16+a*4))
+
+ }
+
+ calcAvergeDogAges(dogAges)
+ console.log(humanDogs);
+ 
+ const hh= humanDogs.filter(aa=>aa>=35)
+ console.log(hh);
+
+ 
+const reducer = (previousValue, currentValue) => previousValue /currentValue;
+
+humanDogs.reduce(reducer);
